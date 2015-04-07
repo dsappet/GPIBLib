3e8babd560b68bf96a22eb55cffca84412c981e2
dsappet <dsappet@gmail.com>
2015-04-07 16:47:20 -0400

    Added ReadMe txt file. Updated the buffSize in Device class to be a variable with get/set rather than function calls for aquiring it.

---
+++ GPIBLib/GPIBLib.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NationalInstruments.VisaNS;
+
+namespace GPIBLib
+{
+    class GPIBException : Exception
+    {
+        // ..  The following three constructors are recommended as a minimum by msdn
+        public GPIBException() { }
+        public GPIBException(string message) : base(message) { }
+        public GPIBException(string message, Exception inner) : base(message, inner) { }
+    }
+    public class Device
+    {
+        // variables
+        private GpibSession mbSession;  // contained in NationalInstruments VisaNS
+        public string manufacturer;
+        public string model;
+        public string address;
+        // variables with accessors
+        public int buffSize
+        {
+            get
+            {
+                int val;
+                if (mbSession != null)
+                {
+                    return mbSession.DefaultBufferSize;
+                }
+                else
+                {
+                    Init();
+                    val = mbSession.DefaultBufferSize;
+                    Close();
+                    return val;
+                }
+            }
+            set
+            {
+                {
+                    try
+                    {
+                        mbSession.DefaultBufferSize = value;
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+        }
+
+        // Constructors
+        public Device() { }
+        public Device(string _mfr, string _model, string _addr)
+        {
+            manufacturer = _mfr;
+            model = _model;
+            address = _addr;
+        }
+        
+        public void Init()
+        {
+            try
+            {
+                if (mbSession != null) mbSession.Dispose();
+                mbSession = new GpibSession(this.address);            
+            }
+            catch (Exception ex)
+            {
+                // Exception occured 
+                throw new GPIBException("inner exception occured in GPIBLib at Init()." , ex);
+            }
+        }
+        public void Close()
+        {
+            try
+            {
+                if (mbSession != null) mbSession.Dispose();
+            }
+            catch (Exception ex) {
+                throw new GPIBException("inner exception occured in GPIBLib at Close()." , ex);
+            }
+        }
+        public string Query(string command)
+        {
+            string reply = string.Empty;
+            try
+            {
+                reply = mbSession.Query(command + "\n");
+            }
+            catch (Exception ex) {
+                throw new GPIBException("inner exception occured in GPIBLib at Query()." , ex);
+            }
+            return reply;
+        }
+        public void WriteOnly(string command)
+        {
+            try
+            {
+                mbSession.Write(command + "\n");
+            }
+            catch (Exception ex) { //
+                throw new GPIBException("inner exception occured in GPIBLib at WriteOnly()." , ex);
+            }
+        }
+        public string ReadOnly()
+        {
+            string reply = string.Empty;
+            try
+            {
+                reply = mbSession.ReadString();
+            }
+            catch (Exception ex)
+            {
+                //
+                throw new GPIBException("inner exception occured in GPIBLib at ReadOnly().", ex);
+            }
+            return reply;
+        }
+        public void changeTimeout(int timeMs)
+        {
+            mbSession.Timeout = timeMs;
+        }
+        public StatusByteFlags SerialPoll()
+        {
+            return mbSession.ReadStatusByte();          
+        }
+        public bool messageAvailable()
+        {
+            StatusByteFlags flags;
+            try
+            {
+                flags = mbSession.ReadStatusByte();
+            }
+            catch (Exception ex)
+            {
+                throw new GPIBException("inner exception occured in GPIBLib @ messageAvailable(). INNER: ", ex);
+            }
+            if (flags.HasFlag(StatusByteFlags.MessageAvailable)) return true;
+            else return false;
+        }
+
+    }
+    public class GPIB
+    {
+        public List<Device> FindInstruments()
+        {
+            List<Device> resources = new List<Device>();
+            ResourceManager manager = ResourceManager.GetLocalManager();
+            string[] resourceList = manager.FindResources("GPIB?*");
+            foreach (string a in resourceList)
+            {
+                HardwareInterfaceType intFaceType;
+                short intFaceNum;
+                string resourceClass = string.Empty;
+                string reply = string.Empty;
+                manager.ParseResource(a, out intFaceType, out intFaceNum, out resourceClass);
+                if (resourceClass == "INTFC")
+                {
+                    try
+                    {
+                        GpibInterface gFace = new GpibInterface(a);
+                        gFace.SendInterfaceClear();
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        // The INTFC cannot be init. 
+                        // Throw an Exception here
+                        //throw new GPIBException("inner exception occured in GPIBLib at FindInstruments() the INTFC cannot be init. " + ex);
+                    }
+                    catch (Exception ex)
+                    {
+                        // Other exception occured 
+                        //throw new GPIBException("inner exception occured in GPIBLib at FindInstruments()." + ex);
+                    }
+                }
+                else if (resourceClass == "INSTR")
+                {
+                    try
+                    {
+                        GpibSession sesh = new GpibSession(a);
+                        reply = sesh.Query("*IDN?\n");
+                        string[] info = reply.Split(',');
+                        resources.Add(new Device(info[0], info[1], a));
+                        sesh.Dispose();
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new GPIBException("Inner exception occured in GPIBLib at FindInstruments(): " + ex);
+                    }
+                }
+            }
+            return resources;
+        }
+    }
+}
---
+++ GPIBLib/ReadMe.txt
@@ -0,0 +1,17 @@
+﻿GPIBLib by Douglas Sappet (dsappet@gmail.com)
+
+NOTICE: Documentation not complete. It's on my TODO list. This document provides a top level overview. 
+
+Overview: 
+The idea behind this library is to compile commonly repeated code in GPIB apps into one re-usable lib. This uses the NationalInstuments VISA.NS library but does not use National Instruments GPIB library. The NI GPIB library only works for National Instruments usb-gpib adapters. This LIB shall work for others such as Agilent as long as their appropriate drivers are installed.
+A class called Device is provided to represent a device on the GPIB bus you wish to talk to. A separate instance shall be used for each instrument. 
+
+The Device Class
+- This class is added to represent a specific device on the GPIB bus.
+- A separate instance of this class should be used for each instance. 
+- This class can be initialized by the GPIB class functions described below but the address element is a public memeber and can be set manually if so desired. 
+- Init / Query / WriteOnly / Read functions provided in the Device class. 
+
+The GPIB Class
+- Contains a function called FindInstruments that returns a list of the device class for each device on the bus. 
+- If possible the FindInstruments function performs an *IDN? request of the instrument and parses the manuf and model information then populates that into the device class. 
